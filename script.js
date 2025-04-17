23f279c9b5a2a06678a7002e6a649520b68117a4
RedRain0o0 <96890698+RedRain0o0@users.noreply.github.com>
2025-04-16 20:06:25 -0400

    random splash on page reload

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-function getSplash() {
+async function getSplash() {
   console.log("Picking random splash");
-  const url = "https://raw.githubusercontent.com/RedRain0o0/redrain0o0.github.io/refs/heads/main/assets/splashes.txt"
-  fetch(url)
-     .then( r => r.text() )
-     //.then( t => //process your text! )
-
+  const url = "https://raw.githubusercontent.com/RedRain0o0/redrain0o0.github.io/refs/heads/main/assets/splashes.txt";
+  let x = await fetch(url);
+  let y = await x.text();
+  //console.log(y);
+  var array = y.split('\r\n');
+  console.log("There are %s splashes", array.length);
+  splash = array[Math.floor(Math.random() * (array.length - 1 + 1) + 0)];
+  console.log(splash);
+  document.getElementById('splashText').innerHTML = splash;
 }
